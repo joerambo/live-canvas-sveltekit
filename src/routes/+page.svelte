51df256f543dbf95ee67a6b31cbd8f0249171ce3
Joe Timmons <info@joetimmons.com>
2023-10-09 00:05:50 -1000

    chore(packages): install tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col justify-center items-center h-full">
+	<h1 class="text-gray-900 text-5xl">Welcome to SvelteKit</h1>
+	<p class="text-gray-700">
+		Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+	</p>
+</div>
