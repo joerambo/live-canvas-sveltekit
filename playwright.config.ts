26d1b446db3295adde5ba7d16894beda078e0707
Joe Timmons <info@joetimmons.com>
2023-10-08 20:30:09 -1000

    chore: update ports and test config

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,8 +2,8 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
+		command: 'pnpm prod',
+		port: 8080
 	},
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+	server: {
+		port: 3000
+	},
+	preview: {
+		port: 8080
+	},
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
